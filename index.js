50c12736d4eb211e1318451a3fb540e41b62db13
Brue Hong <bruce.hong@pncl.tech>
2021-07-24 15:04:53 -0400

    Add nodemon and fix heroku port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const port = process.env.port || 4200;
+const port = process.env.PORT || 4200;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
